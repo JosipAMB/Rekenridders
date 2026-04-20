9681c2399c0ff582948ce93a75679a4327f2229b
JosipAMB <j.ambrozic@student.alfa-college.nl>
2026-04-20 10:26:46 +0200

    level groter gemaakt met meerdere kisten en dood vallen

--- Scenes/Level1Scene.js
+++ Scenes/Level1Scene.js
@@ -1,130 +1,119 @@
-import { genereerSom } from '../Systems/Mathgenerator.js';
 import { HealthSysteem } from '../Systems/Healthsysteem.js';
 import { Chest } from '../Systems/Chest.js';
 
-    class Level1Scene extends Phaser.Scene {
+class Level1Scene extends Phaser.Scene {
     constructor() {
         super('Level1Scene');
-    }   
+    }
 
     preload() {
         this.load.image('background', 'Assets/Background_2.png');
-
-        this.load.spritesheet('idle', 'Assets/IDLE.png', {
-            frameWidth: 96,
-            frameHeight: 84
-        });
-        this.load.spritesheet('run', 'Assets/RUN.png', {
-            frameWidth: 96,
-            frameHeight: 84
-        });
-        this.load.spritesheet('jump', 'Assets/JUMP.png', {
-            frameWidth: 96,
-            frameHeight: 84
-        });
+        this.load.spritesheet('idle', 'Assets/IDLE.png', { frameWidth: 96, frameHeight: 84 });
+        this.load.spritesheet('run', 'Assets/RUN.png', { frameWidth: 96, frameHeight: 84 });
+        this.load.spritesheet('jump', 'Assets/JUMP.png', { frameWidth: 96, frameHeight: 84 });
     }
 
     create() {
         this.spelActief = true;
-        const W = this.scale.width;
-        const H = this.scale.height;
+        this.W = this.scale.width;
+        this.H = this.scale.height;
+        const W = this.W;
+        const H = this.H;
 
         this.health = new HealthSysteem(this);
 
-        // hartjes tekst bovenin scherm
-        this.hpTekst = this.add.text(20, 20, '❤️❤️❤️❤️❤️', {
-            fontSize: '32px'
-        })
-        .setScrollFactor(0);  // vast aan scherm
+        // hartjes
+        this.hpTekst = this.add.text(20, 20, '❤️❤️❤️❤️❤️', { fontSize: '32px' })
+            .setScrollFactor(0);
 
-        // achtergrond eerst
+        // achtergrond
         this.add.image(0, 0, 'background')
             .setOrigin(0, 0)
-            .setDisplaySize(W, H);
+            .setDisplaySize(4000, H)
+            .setScrollFactor(0.5);  // beweegt mee maar langzamer dan camera
 
         // platforms
         this.platforms = this.physics.add.staticGroup();
-        this.platforms.create(W / 2, H - 10, null)
-            .setDisplaySize(W, 40)
-            .setTint(0x8B4513)
-            .refreshBody();
-        this.platforms.create(300, H - 150, null)
-            .setDisplaySize(200, 20)
-            .setTint(0x8B4513)
-            .refreshBody();
-        this.platforms.create(600, H - 250, null)
-            .setDisplaySize(200, 20)
-            .setTint(0x8B4513)
-            .refreshBody();
-        this.platforms.create(200, H - 350, null)
-            .setDisplaySize(200, 20)
+
+        // grond
+        this.platforms.create(2000, H - 10, null)
+            .setDisplaySize(4000, 40)
             .setTint(0x8B4513)
             .refreshBody();
 
-        // speler NA platforms
+        // platforms
+        this.platforms.create(400, H - 150, null).setDisplaySize(200, 20).setTint(0x8B4513).refreshBody();
+        this.platforms.create(700, H - 250, null).setDisplaySize(200, 20).setTint(0x8B4513).refreshBody();
+        this.platforms.create(1000, H - 150, null).setDisplaySize(200, 20).setTint(0x8B4513).refreshBody();
+        this.platforms.create(1300, H - 300, null).setDisplaySize(200, 20).setTint(0x8B4513).refreshBody();
+        this.platforms.create(1600, H - 200, null).setDisplaySize(200, 20).setTint(0x8B4513).refreshBody();
+        this.platforms.create(1900, H - 350, null).setDisplaySize(200, 20).setTint(0x8B4513).refreshBody();
+
+        // speler
         this.player = this.physics.add.sprite(100, H - 150, 'idle');
         this.player.setScale(2);
         this.player.body.setSize(28, 30);
         this.player.body.setOffset(28, 30);
         this.player.play('idle');
 
-        // camera NA speler
+        // camera
         this.cameras.main.startFollow(this.player);
 
         // colliders
         this.physics.add.collider(this.player, this.platforms);
 
-        // kist
-        this.chest = new Chest(this, 400, H - 70);
-        this.physics.add.collider(this.player, this.chest.image);
+        // meerdere kisten
+        this.kisten = [
+            new Chest(this, 400, H - 70),
+            new Chest(this, 1000, H - 70),
+            new Chest(this, 1600, H - 70),
+        ];
+        this.kisten.forEach(kist => {
+            this.physics.add.collider(this.player, kist.image);
+        });
 
         // toetsen
         this.cursors = this.input.keyboard.createCursorKeys();
         this.keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
 
         // animaties
-        this.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('idle', { start: 0, end: 6 }),
-            frameRate: 4,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'run',
-            frames: this.anims.generateFrameNumbers('run', { start: 0, end: 7 }),
-            frameRate: 5,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'jump',
-            frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 4 }),
-            frameRate: 8,
-            repeat: 0
-        });
-        
-        // deur naar eindbaas
-        this.deur = this.physics.add.staticImage(1200, H - 70, null)
+        this.anims.create({ key: 'idle', frames: this.anims.generateFrameNumbers('idle', { start: 0, end: 6 }), frameRate: 4, repeat: -1 });
+        this.anims.create({ key: 'run', frames: this.anims.generateFrameNumbers('run', { start: 0, end: 7 }), frameRate: 5, repeat: -1 });
+        this.anims.create({ key: 'jump', frames: this.anims.generateFrameNumbers('jump', { start: 0, end: 4 }), frameRate: 8, repeat: 0 });
+
+        // deur
+        this.deur = this.physics.add.staticImage(3800, H - 70, null)
             .setDisplaySize(60, 80)
             .setTint(0xff0000)
             .refreshBody();
 
         this.physics.add.overlap(this.player, this.deur, () => {
-            console.log('HP bij deur:', this.health.huidigHp);
             this.scene.start('BossScene', { hp: this.health.huidigHp });
         });
     }
 
     update() {
         const inLucht = !this.player.body.blocked.down;
+        const H = this.H;
 
-        const bijKist = Phaser.Math.Distance.Between(
-            this.player.x, this.player.y,
-            this.chest.image.x, this.chest.image.y  
-        ) < 80;
-
-        if (bijKist && Phaser.Input.Keyboard.JustDown(this.keyE)) {
-            this.openKist();
+        // dood vallen
+        if (this.player.y > H + 100) {
+            this.health.schade();
+            this.player.setPosition(100, H - 150);
+            this.updateHp();
         }
+
+        // kisten checken
+        this.kisten.forEach(kist => {
+            const bijKist = Phaser.Math.Distance.Between(
+                this.player.x, this.player.y,
+                kist.image.x, kist.image.y
+            ) < 80;
+
+            if (bijKist && Phaser.Input.Keyboard.JustDown(this.keyE)) {
+                kist.open(this.health, () => this.updateHp());
+            }
+        });
 
         if (this.cursors.left.isDown) {
             this.player.setVelocityX(-200);
@@ -161,10 +150,6 @@ import { Chest } from '../Systems/Chest.js';
                 { fontSize: '64px', fill: '#ff0000', backgroundColor: '#000000', padding: { x: 20, y: 10 } }
             ).setOrigin(0.5).setScrollFactor(0);
         }
-    }
-
-    openKist() {
-        this.chest.open(this.health, () => this.updateHp());
     }
 }
 
